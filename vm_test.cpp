9c6fc679007a0b78b0c0563f21b860b536da1334
Chalisa Udompanyawit <cudompan@ece021.ece.local.cmu.edu>
2022-11-14 22:04:24 -0500

    reimplemented the TLB using set bits from the VPN new

--- vm_test.cpp
+++ vm_test.cpp
@@ -11,32 +11,32 @@ int main(int argc, char *argv[]){
   void *p = (void*)0x0; 
   vm->vmMap((unsigned long)p,8192);
   
-  std::cerr << "[TEST1: OK; page fault]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+0,0xfeefee);
+  // std::cerr << "[TEST1: OK; page fault]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+0,0xfeefee);
   
-  std::cerr << "[TEST2: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+0,0xfeefee);
+  // std::cerr << "[TEST2: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+0,0xfeefee);
 
-  std::cerr << "[TEST3: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+9,0xfeefee);
+  // std::cerr << "[TEST3: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+9,0xfeefee);
   
-  std::cerr << "[TEST4: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+4095,0xfeefee);
+  // std::cerr << "[TEST4: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+4095,0xfeefee);
   
-  std::cerr << "[TEST5: OK; page fault]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+4096,0xfeefee);
+  // std::cerr << "[TEST5: OK; page fault]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+4096,0xfeefee);
   
-  std::cerr << "[TEST6: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+4096,0xfeefee);
+  // std::cerr << "[TEST6: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+4096,0xfeefee);
   
-  std::cerr << "[TEST7: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+4097,0xfeefee);
+  // std::cerr << "[TEST7: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+4097,0xfeefee);
   
-  std::cerr << "[TEST8: OK; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+5097,0xfeefee);
+  // std::cerr << "[TEST8: OK; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+5097,0xfeefee);
 
-  std::cerr << "[TEST: SEGFAULT; ]" << std::endl;
-  vm->pageStoreSingle((unsigned long)p+8193,0xfeefee);
+  // std::cerr << "[TEST: SEGFAULT; ]" << std::endl;
+  // vm->pageStoreSingle((unsigned long)p+8193,0xfeefee);
   
   // p = (void*)0x18000; 
   // vm->vmMap((unsigned long)p,65536);
@@ -44,17 +44,13 @@ int main(int argc, char *argv[]){
   //   vm->pageStoreSingle((unsigned long)p+i,0xfeefee);
   // }
 
-  // p = (void*)0x18000; 
-  // vm->vmMap((unsigned long)p,65536);
-  // for(int i = 0; i < 65536; i+=1024){
-  //   vm->pageStoreSingle((unsigned long)p+i,0xfeefee);
-  // }
+  p = (void*)0x18000; 
+  vm->vmMap((unsigned long)p,65536);
+  for(int i = 0; i < 65536; i+=1024){
+    vm->pageStoreSingle((unsigned long)p+i,0xfeefee);
+  }
 
 
-
-  
-
-  
   unsigned accesses = vm->getNumAcc();
   unsigned TLB_hits = vm->getNumTlbHit();
   unsigned TLB_misses = vm->getNumTlbMiss();
